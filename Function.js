e3a53588ac512ea0331c555a8f7630246ac2ad54
n-ce <abrocks1120@gmail.com>
2021-02-03 18:50:44 +0530

    Unplugged UI.

--- Function.js
+++ Function.js
@@ -1,26 +1,26 @@
 var y;
-  var b;
-  function H2D(){
-    var x = document.getElementById("HexInput").value;
-    var y = parseInt(x,16);
-  document.getElementById("DecShow").innerHTML = y;
-  }
-  function D2H(){
-    var a = document.getElementById("DecInput").value;
-    var b = Number(a).toString(16);
-    document.getElementById("HexShow").innerHTML = b;
-  }
-  function CopyH(){
-    var a = document.getElementById("DecInput");
-    var c = Number(a).toString(16);
-    c.select();
-    c.setSelectionRange(0, 99999)
-    document.execCommand("copy");
-    alert("Copied the text: " + c.value);
-  }
-  function CopyD(){
-  y.select();
-  y.setSelectionRange(0, 99999)
-  document.execCommand("copy");
-  alert("Copied the text: " + y.value);
-  }
+var b;
+
+function H2D(){
+	var x = document.getElementById("HexInput").value;
+	var y = parseInt(x,16);
+	document.getElementById("DecShow").innerHTML = y;}
+	
+function D2H(){
+	var a = document.getElementById("DecInput").value;
+	var b = Number(a).toString(16);
+	document.getElementById("HexShow").innerHTML = b;}
+	
+function CopyH(){
+	var a = document.getElementById("DecInput");
+	var c = Number(a).toString(16);
+	c.select();
+	c.setSelectionRange(0, 99999);
+	document.execCommand("copy");
+	alert("Copied the text: " + c.value);}
+	
+function CopyD(){
+	y.select();
+	y.setSelectionRange(0, 99999);
+	document.execCommand("copy");
+	alert("Copied the text: " + y.value);}
