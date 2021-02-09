bf75c43f25fc7f909c262b8a9558e5c63aca9889
n-ce <abrocks1120@gmail.com>
2021-02-09 18:41:56 +0530

    pt2

--- Function.js
+++ Function.js
@@ -1,3 +1,20 @@
+document.onreadystatechange = function() {
+
+  if (document.readyState !== "complete") {
+
+    document.querySelector("body").style.visibility = "hidden";
+
+    document.querySelector("#loader").style.visibility = "visible";
+
+  } else {
+
+    document.querySelector("#loader").style.display = "none";
+
+    document.querySelector("body").style.visibility = "visible";
+
+  }
+};
+
 function H2D(){
 	var x = document.getElementById("Input").value;
 	var y = parseInt(x,16);
