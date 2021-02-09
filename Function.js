63fef1ff847fa10b78eaf7ba29cf0572a58c916d
n-ce <abrocks1120@gmail.com>
2021-02-09 13:31:09 +0530

    UI completion

--- Function.js
+++ Function.js
@@ -1,26 +1,9 @@
-var y;
-var b;
-
 function H2D(){
-	var x = document.getElementById("HexInput").value;
+	var x = document.getElementById("Input").value;
 	var y = parseInt(x,16);
-	document.getElementById("DecShow").innerHTML = y;}
+	document.getElementById("Show").innerHTML = y;}
 	
 function D2H(){
-	var a = document.getElementById("DecInput").value;
+	var a = document.getElementById("Input").value;
 	var b = Number(a).toString(16);
-	document.getElementById("HexShow").innerHTML = b;}
-	
-function CopyH(){
-	var a = document.getElementById("DecInput");
-	var c = Number(a).toString(16);
-	c.select();
-	c.setSelectionRange(0, 99999);
-	document.execCommand("copy");
-	alert("Copied the text: " + c.value);}
-	
-function CopyD(){
-	y.select();
-	y.setSelectionRange(0, 99999);
-	document.execCommand("copy");
-	alert("Copied the text: " + y.value);}
+	document.getElementById("Show").innerHTML = b;}
