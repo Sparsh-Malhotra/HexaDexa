a20cca753f254247ef2be2ed103ee6697715d330
n-ce <abrocks1120@gmail.com>
2021-02-10 09:58:53 +0530

    PWA

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const staticLoader = "HexaDexa"
+const assets = [
+  "/",
+  "index.html",
+  "UI.css",
+  "192px_icon.png",
+  "512px_icon.png"
+]
+
+self.addEventListener("install", installEvent => {
+	installEvent.waitUntil(
+		caches.open(staticLoader).then(cache => {
+			cache.addAll(assets)
+		})
+	)
+})
+self.addEventListener("fetch", fetchEvent => {
+	fetchEvent.respondWith(
+		caches.match(fetchEvent.request).then(res => {
+			return res || fetch(fetchEvent.request)
+		})
+	)
+})
