865e1f705a03d71de3d3aa6aa8606e1888c8a414
n-ce <abrocks1120@gmail.com>
2021-05-15 16:29:19 +0530

    Complete UI Overhaul

--- Function.js
+++
@@ -1,9 +0,0 @@
-function H2D(){
-	var x = document.getElementById("Input").value;
-	var y = parseInt(x,16);
-	document.getElementById("Show").innerHTML = y;}
-	
-function D2H(){
-	var a = document.getElementById("Input").value;
-	var b = Number(a).toString(16);
-	document.getElementById("Show").innerHTML = b;}
