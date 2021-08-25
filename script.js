9ccc5e95ead8f31ef6d19c1e2c7e1e6d41751867
n-ce <abrocks1120@gmail.com>
2021-08-25 20:49:25 +0530

    external script

---
+++ script.js
@@ -0,0 +1,6 @@
+function H2D(){
+			  document.querySelector('p').innerHTML = parseInt(document.querySelector('input').value,16);
+}
+function D2H(){
+			  document.querySelector('p').innerHTML = Number(document.querySelector('input').value).toString(16);
+}
