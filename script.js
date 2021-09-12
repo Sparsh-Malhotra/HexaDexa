a1af4dbd4fac810d61375aa6dc632c977a6f5309
Sparsh Malhotra <sparshmalhotraaa@gmail.com>
2021-09-12 12:45:18 +0530

    Implemented Copy to clipboard function

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-var output = document.querySelector('p');
+var output = document.querySelector("p");
 
-var input = document.querySelector('input');
+var input = document.querySelector("input");
 
-function H2D(){
-  output.innerHTML = parseInt(input.value,16);
+var message = document.querySelector(".tooltiptext");
+
+function H2D() {
+  output.innerHTML = parseInt(input.value, 16);
 }
-function D2H(){
+function D2H() {
   output.innerHTML = Number(input.value).toString(16);
+}
+function copyToClipboard() {
+  navigator.clipboard.writeText(output.textContent);
+  message.textContent = "Copied Successfully";
+}
+function textCopied() {
+  message.textContent = "Copy to Clipboard";
 }
